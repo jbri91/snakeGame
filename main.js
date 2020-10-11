47b04e6e893891ccc5ce7dcaa0bd5fedfa05ebb7
Johnny Briones <jbrrii91@gmail.com>
2020-10-10 23:03:41 -0400

    draw functions

--- main.js
+++ main.js
@@ -1,16 +1,27 @@
 let canvas;
 let canvasContext;
+let appleX = 50;
+let appleY;
 
 window.onload = function () {
-    canvas = document.getElementById('gameCanvas');
-    canvasContext = canvas.getContext('2d');
-    canvasContext.fillStyle = 'black';
-    canvasContext.fillRect(0,0,canvas.clientWidth,canvas.height);
+  canvas = document.getElementById("gameCanvas");
+  canvasContext = canvas.getContext("2d");
+  drawEverything();
+};
 
-    canvasContext.fillStyle = 'green';
-    canvasContext.fillRect(200,300,200,20);
-
-    canvasContext.fillStyle = 'red';
-    canvasContext.fillRect(250,350,10,10);
+function drawEverything() {
+  canvasContext.fillStyle = "black";
+  canvasContext.fillRect(0, 0, canvas.clientWidth, canvas.height);
+  drawSnake();
+  drawApple();
 }
 
+function drawSnake() {
+  canvasContext.fillStyle = "green";
+  canvasContext.fillRect(200, 300, 200, 20);
+}
+
+function drawApple() {
+  canvasContext.fillStyle = "red";
+  canvasContext.fillRect(appleX, 350, 10, 10);
+}
