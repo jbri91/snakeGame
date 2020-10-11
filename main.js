8b3e624ccfba77798b3347b8a8da1f92b1dcb7a2
Johnny Briones <jbrrii91@gmail.com>
2020-10-10 23:14:53 -0400

    framesPerSecond

--- main.js
+++ main.js
@@ -2,11 +2,14 @@ let canvas;
 let canvasContext;
 let appleX = 50;
 let appleY;
+let snakeX = 50;
+let snakeY;
 
 window.onload = function () {
   canvas = document.getElementById("gameCanvas");
   canvasContext = canvas.getContext("2d");
-  drawEverything();
+  let framesPerSecond = 30;
+  setInterval(drawEverything, 1000/framesPerSecond);
 };
 
 function drawEverything() {
@@ -17,11 +20,13 @@ function drawEverything() {
 }
 
 function drawSnake() {
+  snakeX = snakeX + 10;
   canvasContext.fillStyle = "green";
-  canvasContext.fillRect(200, 300, 200, 20);
+  canvasContext.fillRect(snakeX, 300, 200, 20);
 }
 
 function drawApple() {
+  appleX = appleX + 10;
   canvasContext.fillStyle = "red";
   canvasContext.fillRect(appleX, 350, 10, 10);
 }
