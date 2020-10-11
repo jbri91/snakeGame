3b243e4956d2459b0d5f572ae27cfadfc2f48e8c
Johnny Briones <jbrrii91@gmail.com>
2020-10-10 23:31:18 -0400

    move functions

--- main.js
+++ main.js
@@ -4,12 +4,17 @@ let appleX = 50;
 let appleY;
 let snakeX = 50;
 let snakeY;
+let snakeSpeed = 5;
 
 window.onload = function () {
   canvas = document.getElementById("gameCanvas");
   canvasContext = canvas.getContext("2d");
   let framesPerSecond = 30;
-  setInterval(drawEverything, 1000/framesPerSecond);
+  setInterval(function () {
+    moveSnake();
+    moveApple();
+    drawEverything();
+  }, 1000 / framesPerSecond);
 };
 
 function drawEverything() {
@@ -19,14 +24,25 @@ function drawEverything() {
   drawApple();
 }
 
+function moveSnake() {
+  snakeX = snakeX + snakeSpeed;
+  if (snakeX > 600) {
+    snakeSpeed = -5;
+  } else if (snakeX < 0) {
+    snakeSpeed = 5;
+  }
+}
+
+function moveApple() {
+    appleX = appleX + 5;
+}
+
 function drawSnake() {
-  snakeX = snakeX + 10;
   canvasContext.fillStyle = "green";
   canvasContext.fillRect(snakeX, 300, 200, 20);
 }
 
 function drawApple() {
-  appleX = appleX + 10;
   canvasContext.fillStyle = "red";
   canvasContext.fillRect(appleX, 350, 10, 10);
 }
