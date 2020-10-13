e0dc693501d8d13a1db66d5b3adef918a1b42a09
Johnny Briones <jbrrii91@gmail.com>
2020-10-13 11:52:39 -0400

    snake object

--- main.js
+++ main.js
@@ -9,17 +9,16 @@ let snakeX = 400;
 let snakeY = 300;
 let snakeLength = 20;
 let snakeWidth = 20;
-let snakeSpeed = 5;
+let snakeSpeed = 10;
 let showingLoseScreen = false;
 let rightPressed = false;
 let leftPressed = false;
 let upperPressed = false;
 let lowerPressed = false;
-score = document.getElementById("score");
-highScore = document.getElementById("highScore");
+const score = document.getElementById("score");
+const highScore = document.getElementById("highScore");
 
 document.addEventListener("keydown", keyDownHandler, false);
-// document.addEventListener("keyup", keyUpHandler, false);
 
 function keyDownHandler(e) {
   if (e.key == "Right" || e.key == "ArrowRight") {
@@ -40,24 +39,11 @@ function keyDownHandler(e) {
     rightPressed = false;
   } else if (e.key == "Down" || e.key == "ArrowDown") {
     lowerPressed = true;
-    upperpressed = false;
+    upperPressed = false;
     leftPressed = false;
     rightPressed = false;
   }
 }
-
-// function keyUpHandler(e) {
-//   if (e.key == "Right" || e.key == "ArrowRight") {
-//     rightPressed = false;
-//   } else if (e.key == "Left" || e.key == "ArrowLeft") {
-//     leftPressed = false;
-//   }
-//   if (e.key == "Up" || e.key == "ArrowUp") {
-//     upperPressed = false;
-//   } else if (e.key == "Down" || e.key == "ArrowDown") {
-//     lowerPressed = false;
-//   }
-// }
 
 window.onload = function () {
   canvas = document.getElementById("gameCanvas");
@@ -79,18 +65,16 @@ function drawEverything() {
 }
 
 function moveSnake() {
-
-
-  if (rightPressed) {
-    snakeX += 10;
+    if (rightPressed) {
+    snakeX += snakeSpeed;
   } else if (leftPressed) {
-      snakeX -= 10;
+      snakeX -= snakeSpeed;
   }
 
   if (upperPressed) {
-    snakeY -= 10;
+    snakeY -= snakeSpeed;
   } else if (lowerPressed) {
-    snakeY += 10;
+    snakeY += snakeSpeed;
     
   }
 
@@ -105,6 +89,13 @@ function moveSnake() {
 }
 
 function moveApple() {}
+
+let snake = {
+  
+  snakeLength: 20,
+  snakeWidth: 20,
+  snakeSpeed: 10,
+}
 
 let snakeBody = [
   { x: 400, y: 300, snakeLength, snakeWidth},
